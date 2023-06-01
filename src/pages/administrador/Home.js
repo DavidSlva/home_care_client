eb60de942a1ee5137108eaa2d126ac34d1ff39f3
castrometro <pablo.castro_d@mail.udp.cl>
2023-06-01 15:38:02 -0400

    Se agregaron a las card, los enlaces

--- src/pages/administrador/Home.js
+++ src/pages/administrador/Home.js
@@ -1,32 +1,38 @@
 import React from 'react'
 import { Card, Col, Row } from 'antd';
+import { Link } from 'react-router-dom';
 const { Meta } = Card;
 const cards = [
   {
     title: 'Administrar Personal de Salud',
     description: 'Agregar/Eliminar Personal',
-    img: "https://www.uchile.cl/.imaging/stk/uchile/nav-lateral-ancho/dam/imagenes/Salud-publica/imagenes-noticias/162548_1_personal-sanitario_L/jcr:content/personal-sanitario.jpg.jpg"
+    img: "https://www.uchile.cl/.imaging/stk/uchile/nav-lateral-ancho/dam/imagenes/Salud-publica/imagenes-noticias/162548_1_personal-sanitario_L/jcr:content/personal-sanitario.jpg.jpg",
+    enlace: "/login"
   },
   {
     title: 'Ver Personal de Salud Actual',
     description: 'Horarios, registros, contacto, etc',
-    img: "https://cindercapacitacion.com/wp-content/uploads/2022/07/cursos-para-trabajadores-de-la-salud-1.jpg"
+    img: "https://cindercapacitacion.com/wp-content/uploads/2022/07/cursos-para-trabajadores-de-la-salud-1.jpg",
+    enlace: "/login"
   },
   {
     title: 'Ver Pacientes',
     description: 'Ver registro de pacientes actuales',
-    img: "https://www.promedco.com/images/NOTICIAS_2020/comodidad-del-paciente-1.jpg"
-   },
-   {
+    img: "https://www.promedco.com/images/NOTICIAS_2020/comodidad-del-paciente-1.jpg",
+    enlace: "/login"
+  },
+  {
     title: 'Generador de Horarios',
     description: 'Genera horarios de visita para el personal de salud',
-    img: "https://cdn-icons-png.flaticon.com/512/425/425868.png"
-   },
-   {
+    img: "https://cdn-icons-png.flaticon.com/512/425/425868.png",
+    enlace: "/login"
+  },
+  {
     title: 'Gestion Financiera',
     description: 'Registro de pagos, facturas, etc',
-    img:"https://www.unap.cl/prontus_unap/site/artic/20140909/imag/foto_0000000220140909133126/finanzas_institucionales.jpg"   
-   }
+    img:"https://www.unap.cl/prontus_unap/site/artic/20140909/imag/foto_0000000220140909133126/finanzas_institucionales.jpg",
+    enlace: "/login"   
+  }
    
 ]
 const Home = () => (
@@ -35,6 +41,7 @@ const Home = () => (
       {
         cards?.map((card) => (
         <Col span={8}>
+        <Link to={card.enlace}>
         <Card className='card__hover'
               cover={
                 <img
@@ -50,6 +57,7 @@ const Home = () => (
           description={card.description}
         />
         </Card>
+        </Link>
         </Col>
         ))
       }
