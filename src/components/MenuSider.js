c160d152ea9510bb111f2dc84da48e32c0bd9aaf
castrometro <pablo.castro_d@mail.udp.cl>
2023-06-28 16:00:35 -0400

    kkj

--- src/components/MenuSider.js
+++ src/components/MenuSider.js
@@ -2,42 +2,7 @@ import { CalendarOutlined, DashboardOutlined, UserOutlined } from '@ant-design/i
 import { Menu } from 'antd'
 import React from 'react'
 const menu = Object.freeze({
-    'especialista': [
-        {
-            label: 'Inicio',
-            key: 'inicio',
-            icon: <DashboardOutlined/>
-        },
-        {
-            label: 'Calendario',
-            key: 'calendario',
-            icon: <CalendarOutlined/>
-        }
-    ],
-    'administrador': [
-        {
-            label: 'Inicio',
-            key: 'inicio',
-            icon: <DashboardOutlined/>
-        },
-        {
-            label: 'Pages',
-            key: 'pages',
-            icon: <UserOutlined/>,
-            children: [
-                {
-                    label: 'Dashboard',
-                    key: 'dashboard2',
-                    icon: <DashboardOutlined/>
-                },
-                {
-                    label: 'Dashboard',
-                    key: 'dashboard3',
-                    icon: <DashboardOutlined/>
-                },
-            ]
-        }
-    ]
+    
 })
 
 const MenuSider = () => {
